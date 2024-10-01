10658aeda32c21d25f8604ea184f7228f807c45d
jotieno <jotieno@jambopay.co.ke>
2024-10-01 14:08:04 +0300

    working on my first screen, amazing

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center">
-    <TouchableOpacity className="bg-teal-500 p-3 rounded-lg">
-      <Text className="text-cyan-800">Click Me</Text>
-    </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
